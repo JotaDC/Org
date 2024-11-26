53d5bebea5064eff19f88c2ae403e607ed95d482
JotaDC <javierdariocanete@gmail.com>
2024-11-26 19:03:19 -0300

    componente MiOrg

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './componentes/Header/Header';
 import Formulario from './componentes/Formulario/Formulario';
+import MiOrg from './componentes/MiOrg';
 
 function App() {
   return (
     <div>
       <Header />
       <Formulario />
-      
+      <MiOrg />
     </div>
   );
 }
