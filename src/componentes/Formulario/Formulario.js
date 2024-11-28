7e7320eb5d91f4c85da1b8a278d2ca733917eb98
JotaDC <javierdariocanete@gmail.com>
2024-11-28 17:27:00 -0300

    controlando inputs

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -1,22 +1,50 @@
+import { useState } from "react"
 import "./Formulario.css"
 import CampoTexto from "../CampoTexto"
 import ListaOpciones from "../ListaOpciones"
 import Boton from "../Boton"
 
 const Formulario =()=>{
-
-    const manejarEnvio=(event)=>{
-        event.preventDefault()
-        console.log("Manejar Envio: ",event)
+    const [nombre,actualizarNombre]=useState("")
+    const [puesto,actualizarPuesto]=useState("")
+    const [foto,actualizarFoto]=useState("")
+    
+    const manejarEnvio=(e)=>{
+        e.preventDefault()
+        console.log("Manejar Envio: ")
+        const datosAEnviar={
+            nombre:nombre,
+            puesto:puesto,
+            foto:foto
+        }
+        console.log(datosAEnviar)
         
     }
 
     return  <section className="formulario">
                 <form onSubmit={manejarEnvio}>
                     <h2>Rellena el formulario para crear el colaborador</h2>
-                    <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" required/>
-                    <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" required/>
-                    <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto" required/>
+                    <CampoTexto 
+                        titulo="Nombre" 
+                        placeholder="Ingresar nombre"
+                        required 
+                        valor={nombre} 
+                        actualizarValor={actualizarNombre}
+                    />
+                    <CampoTexto 
+                        titulo="Puesto" 
+                        placeholder="Ingresar puesto" 
+                        required
+                        valor={puesto} 
+                        actualizarValor={actualizarPuesto}
+                    />
+                    <CampoTexto 
+                        titulo="Foto" 
+                        placeholder="Ingresar enlace de foto" 
+                        required
+                        valor={foto}
+                        actualizarValor={actualizarFoto}
+                    />
                     <ListaOpciones />
                     
                     <Boton>Crear</Boton>
