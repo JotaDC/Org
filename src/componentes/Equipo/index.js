b4e312b2e25c5a6f7a9aec587a43f3e74b07d819
JotaDC <javierdariocanete@gmail.com>
2024-12-23 14:59:50 -0300

    color de fondo equipo

--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -11,20 +11,25 @@ const Equipo = (props) => {
     const estiloTitulo = {
         borderColor: colorPrimario
     }
-    return <> {colaboradores.length > 0 &&
-        <section className="equipo" style={obj}>
-            <h3 style={estiloTitulo}>{titulo}</h3>
-            <div className="colaboradores">
+    return <>
+        {colaboradores.length > 0 &&
+            <section className="equipo" style={obj}>
+                <h3 style={estiloTitulo}>{titulo}</h3>
+                <div className="colaboradores">
 
-                {
-                    colaboradores.map((colaborador, index) => {
-                        return <Colaborador datos={colaborador} key={index} />
-                    })
-                }
+                    {
+                        colaboradores.map((colaborador, index) => {
+                            return <Colaborador
+                                datos={colaborador}
+                                key={index}
+                                colorPrimario={colorPrimario}
+                            />
+                        })
+                    }
 
-            </div>
-        </section>
-    }
+                </div>
+            </section>
+        }
     </>
 }
 
